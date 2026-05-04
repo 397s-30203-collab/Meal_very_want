e20b6238fa9c9ac7e47088ca10aa1c5efe034106
397s-30203-collab <397s-30203@cberi.go.kr>
2026-05-04 16:48:29 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="🚦 청주여고 급식실 정거장",
-    page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR19qbmVaS9EBCDvQTwnf_EjdIpU3ZdfUSJNw&s",
+    page_icon="https://school.cbe.go.kr/hosts/cheongjug-h/M01/logo.png",
     layout="wide"
 )
 
