c811c7d9148f0a3de9f3a51db46ca3c32c709c5b
397s-30203-collab <397s-30203@cberi.go.kr>
2026-05-05 13:46:48 +0900

    Update 01_Student.py

--- pages/01_Student.py
+++ pages/01_Student.py
@@ -24,7 +24,7 @@ def fetch_menu(key: str, atpt: str, schul: str, date: str):
     res.raise_for_status()
     return res.json()
 
-today = 20260506
+today = datetime.today().strftime("%Y%m%d")
 
 KEY = "55a38ff473224d2090f4dfc7a0300ed9"
 ATPT = "M10"
